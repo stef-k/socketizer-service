7636a39c2976c52688918e5b33f2136881b1028c
Stef Kariotidis <stef.kariotidis@gmail.com>
2016-07-08 21:05:18 +0300

    deployment script

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# deploy to production server
+server_ip="85.159.213.249"
+
+mkdir -p  "/tmp/socketizer-service/static/js/service/wordpress"
+
+cp "static/js/service/wordpress/socketizer.min.js" "/tmp/socketizer-service/static/js/service/wordpress"
+
+cp "socketizer-service" "/tmp/socketizer-service"
+
+cp "socketizer-service.conf" "/tmp/socketizer-service"
+
+tar -C /tmp/ -czf socketizer-service.tar.gz socketizer-service
+
+scp socketizer-service.tar.gz stef@"$server_ip":/home/stef
+
+rm -r "/tmp/socketizer-service"
+
+rm socketizer-service.tar.gz
+

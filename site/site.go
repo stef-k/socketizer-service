bbe0cc269502b616458ae8abef84f9e753e1ce70
Stef Kariotidis <stef.kariotidis@gmail.com>
2016-07-09 00:21:17 +0300

    correct db port

--- site/site.go
+++ site/site.go
@@ -9,7 +9,7 @@ import (
 
 func InitDB() *gorm.DB {
 	//db, err := gorm.Open("postgres", "socketizer=h5epb4N1shOz5i0AzqQN9zyxzBDMkdavJsTvuUBIui4WjFAIBt@tcp(127.0.0.1:5333)/socketizer")
-	db, err := gorm.Open("postgres", "postgresql://socketizer:h5epb4N1shOz5i0AzqQN9zyxzBDMkdavJsTvuUBIui4WjFAIBt@127.0.0.1:5433")
+	db, err := gorm.Open("postgres", "postgresql://socketizer:h5epb4N1shOz5i0AzqQN9zyxzBDMkdavJsTvuUBIui4WjFAIBt@127.0.0.1:5432")
 
 	if err != nil {
 		fmt.Println(err)
